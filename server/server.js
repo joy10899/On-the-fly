8bb4d6dbdc4f6e4d515a607457040a73cd2d39b4
joy10899 <joytran10899@gmail.com>
2026-07-29 18:47:15 -0700

    update client

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(session({
 }))
 app.use(express.json())
 app.use(cors({
-    origin: 'https://client-jl5g.onrender.com',
+    origin: 'CLIENT_URL',
     methods: 'GET,POST,PUT,DELETE,PATCH',
     credentials: true
 }))
